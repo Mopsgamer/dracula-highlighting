69b3d8d47a0c5e3ea735cdca6888bb05a863f82e
Mopsgamer <79159094+Mopsgamer@users.noreply.github.com>
2026-01-15 16:10:09 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Dracula Highlighting
+
+Modern dracula-highlighting themes for VSCode.
+
+## Themes
+
+1. Dark Modern (Dracula)
+
+![Dark Modern (Dracula) Theme Preview](https://github.com/Mopsgamer/dracula-highlighting/blob/main/assets/dark-modern-dracula.avif)
+
+2. Dark (Dracula)
+
+![Dark (Dracula) Theme Preview](https://github.com/Mopsgamer/dracula-highlighting/blob/main/assets/dark-dracula.avif)
+
+3. Monokai (Dracula)
+
+![Dark (Dracula) Theme Preview](https://github.com/Mopsgamer/dracula-highlighting/blob/main/assets/monokai-dracula.avif)
---
+++ scripts/assets.js
@@ -0,0 +1,30 @@
+import fs from "node:fs";
+import path from "node:path";
+import sharp from "sharp";
+
+const inputDir = "./assets";
+const outputDir = "./assets";
+
+if (!fs.existsSync(outputDir)) {
+  fs.mkdirSync(outputDir, { recursive: true });
+}
+
+const files = fs.readdirSync(inputDir).filter(f => f.endsWith(".png"));
+
+for (const file of files) {
+  const inputPath = path.join(inputDir, file);
+  const outputPath = path.join(
+    outputDir,
+    file.replace(".png", ".avif")
+  );
+
+  await sharp(inputPath)
+    .avif({ quality: 100 })
+    .toFile(outputPath);
+
+  console.log(`Converted ${file}`);
+  fs.rmSync(inputPath)
+  console.log(`Removed ${file}`);
+}
+
+console.log("All images converted");
